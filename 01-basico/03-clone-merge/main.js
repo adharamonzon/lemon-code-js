4c9e45e3efe8653600950ef5a4cd3027f302fa8b
adharamonzon <adharamonzon@gmail.com>
2022-11-08 20:33:57 +0100

    add exercise clone and merge

--- 01-basico/03-clone-merge/main.js
+++ 01-basico/03-clone-merge/main.js
@@ -7,7 +7,23 @@ function Person({ name, age }) {
 function clone(source) {
   const user = new Person(source);
   console.log(user);
+  return user;
 }
 
 clone({ name: 'ana', age: 34 });
 clone({ name: 'silvia', age: 54 });
+
+//02. implementa una fn merge que,dados dos objetos de entrada source y target, devuelva un nuevo objeto con todas las propiedades de target y de source,
+//y en caso de propiedades con el mismo nombre source sobreescribe a target.
+
+const a = { name: 'Maria', surname: 'Ibañez', country: 'SPA' };
+const b = { name: 'Luisa', age: 31, married: true };
+
+function merge(source, target) {
+  //si implementamos primero target, el name que del objeto c será 'Luisa'
+  const c = { ...source, ...target };
+  console.log(c);
+  return c;
+}
+
+merge(a, b);
